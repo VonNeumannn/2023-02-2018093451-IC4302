1e1b2e9c1923446f91e2322ec284f89c0f4da141
JRicardoCardona <jcardonar@estudiantec.cr>
2023-10-08 03:44:51 -0600

    Dockerize

--- P1/templates/vm01.sh
+++ P1/templates/vm01.sh
@@ -18,4 +18,7 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker login
+
 docker run -d -p 80:5000 --name helloflask nereo08/helloflask
