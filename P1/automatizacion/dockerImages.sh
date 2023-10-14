b698e96fc1f55ea9b4f27f341ef42a9ff667661f
JRicardoCardona <jcardonar@estudiantec.cr>
2023-10-13 18:23:35 -0600

    Final Código y terraforms

--- P1/automatizacion/dockerImages.sh
+++ P1/automatizacion/dockerImages.sh
@@ -4,27 +4,27 @@ cd docker
 
 # Se crea la imagen para hacer la infrastructura
 cd autonomousInfrastructure
-sudo docker build -t vonneumannn/autonomous-infrastructure .
+sudo docker build -t jcardonar/autonomous-infrastructure .
 sudo docker images
-sudo docker push vonneumannn/autonomous-infrastructure
+sudo docker push jcardonar/autonomous-infrastructure
 
 # Se crea la imagen del componente Loader
 cd ..
 cd Loader
-sudo docker build -t vonneumannn/wiki-loader .
+sudo docker build -t jcardonar/wiki-loader .
 sudo docker images
-sudo docker push vonneumannn/wiki-loader
+sudo docker push jcardonar/wiki-loader
 
 # Imagen para crear el API
 cd .. 
 cd API
-sudo docker build -t vonneumannn/api-wikidb .
+sudo docker build -t jcardonar/api-wikidb .
 sudo docker images
-sudo docker push vonneumannn/api-wikidb
+sudo docker push jcardonar/api-wikidb
 
 # Imagen para crear el componente Web
 cd ..
 cd UI
-sudo docker build -t vonneumannn/docker-react .
+sudo docker build -t jcardonar/docker-react .
 sudo docker images
-sudo docker push vonneumannn/docker-react
+sudo docker push jcardonar/docker-react
